03ef6c32ba17595528580c56165ebc32b248bf71
Nidal <nidal@qburst.com>
2012-09-12 15:38:56 +0530

    Almost Complete

---
+++ The Helper/TipDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipDetailViewController.h
+//  The Helper
+//
+//  Created by qbadmin on 9/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipDetailViewController : UIViewController
+
+@end
---
+++ The Helper/TipDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  TipDetailViewController.m
+//  The Helper
+//
+//  Created by qbadmin on 9/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TipDetailViewController.h"
+
+@implementation TipDetailViewController
+
+@end
